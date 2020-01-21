a4f1a71cad135f71c824c808bdd732c2710c0fb4
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-01-21 12:15:22 -0500

    just noticed search dir, completed linear search f(x)

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,10 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search		
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
+    # TO-DO: add missing code
+    for num in arr:
+        if num == target:
+            return True
+    return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
