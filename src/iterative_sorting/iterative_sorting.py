99fe97e965e0a13bd41a98dd5b9d699e06541ea5
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-01-21 15:22:11 -0500

    solved bubble_sort bug

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -28,7 +28,7 @@ def bubble_sort(arr):
     for i in range(n):
         for j in range(0, n-i-1):
             print(arr[j], arr[j+1])
-            if arr[j] > arr[i+1]:
+            if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
